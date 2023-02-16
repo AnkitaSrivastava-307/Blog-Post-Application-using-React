9300f8f21dfe6c56ca425c65e21097c16907c9f2
AnkitaSrivastava-307 <122794123+AnkitaSrivastava-307@users.noreply.github.com>
2023-02-16 18:28:24 +0530

    Add files via upload

---
+++ src/components/Badge.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {MDBBadge} from "mdb-react-ui-kit";
+const Badge = ({children , styleInfo}) => {
+    const colorKey = {
+        Fashion: "primary",
+        Travel: "success",
+        Animal: "danger",
+        Food: "warning",
+        Tech: "info",
+        Sports: "dark"
+    };
+    return (
+        <h5 style={styleInfo}>
+            <MDBBadge color={colorKey[children]}>{children}</MDBBadge>
+        </h5>
+    )
+}
+
+export default Badge;
